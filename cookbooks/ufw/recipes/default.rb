b009c7b245e4a80d93f6475cc08f1d2e1e0eb898
Kohei Hasegawa <ameutau@gmail.com>
2012-07-08 03:22:53 +0900

    add configure for ufw

--- cookbooks/ufw/recipes/default.rb
+++ cookbooks/ufw/recipes/default.rb
@@ -6,3 +6,23 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+package "ufw" do
+  action :upgrade
+end
+
+execute "ufw default deny"
+
+%w(80 443 10022).each do |allow|
+  execute "allow #{allow}" do
+    command "ufw allow #{allow}"
+  end
+end
+
+execute "deny 22" do
+  command "ufw deny 22"
+end
+
+execute "enable UFW" do
+  command "echo y | ufw enable" # "Command may disrupt existing ssh connections. Proceed with operation (y|n)?"
+end
