a88a6d1f1c81dc25098396ea77215af1a7f77e46
Kohei Hasegawa <ameutau@gmail.com>
2012-07-08 03:21:59 +0900

    add xmllibs

---
+++ cookbooks/xmllibs/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: xmllibs
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "libxml2-dev"
+package "libxslt1-dev"
