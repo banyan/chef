8ed4e935b1521c68f2457a0ad660cc35cb9e958a
Kohei Hasegawa <ameutau@gmail.com>
2012-07-08 01:50:43 +0900

    add openssh

---
+++ cookbooks/openssh/recipes/default.rb
@@ -0,0 +1,51 @@
+#
+# Cookbook Name:: openssh
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+packages = case node[:platform]
+  when "centos","redhat","fedora","scientific"
+    %w{openssh-clients openssh}
+  when "arch"
+    %w{openssh}
+  else
+    %w{openssh-client openssh-server}
+  end
+  
+packages.each do |pkg|
+  package pkg
+end
+
+template "/etc/ssh/sshd_config" do
+  owner  "root"
+  group  "root"
+  mode   0644
+  source name
+end
+
+service "ssh" do
+  case node[:platform]
+  when "centos","redhat","fedora","arch","scientific"
+    service_name "sshd"
+  else
+    service_name "ssh"
+  end
+  supports value_for_platform(
+    "debian" => { "default" => [ :restart, :reload, :status ] },
+    "ubuntu" => {
+      "8.04" => [ :restart, :reload ],
+      "default" => [ :restart, :reload, :status ]
+    },
+    "centos" => { "default" => [ :restart, :reload, :status ] },
+    "redhat" => { "default" => [ :restart, :reload, :status ] },
+    "fedora" => { "default" => [ :restart, :reload, :status ] },
+    "scientific" => { "default" => [ :restart, :reload, :status ] },
+    "arch" => { "default" => [ :restart ] },
+    "default" => { "default" => [:restart, :reload ] }
+  )
+  action [ :enable, :start ]
+end
