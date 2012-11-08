874ccef36d2384544312980972b85c1bb4e29a02
Kohei Hasegawa <ameutau@gmail.com>
2012-11-09 02:35:59 +0900

    refinement ufw

---
+++ cookbooks/ufw/attributes/default.rb
@@ -0,0 +1,2 @@
+default[:ufw][:allow] = [ 80, 10022 ]
+default[:ufw][:deny]  = [ 22 ]
--- cookbooks/ufw/recipes/default.rb
+++ cookbooks/ufw/recipes/default.rb
@@ -5,24 +5,14 @@
 # Copyright 2012, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
-#
+
+include_recipe "apt"
 
 package "ufw" do
   action :upgrade
 end
 
-execute "ufw default deny"
-
-%w(80 443 10022).each do |allow|
-  execute "allow #{allow}" do
-    command "ufw allow #{allow}"
-  end
-end
-
-execute "deny 22" do
-  command "ufw deny 22"
-end
-
 execute "enable UFW" do
-  command "echo y | ufw enable" # "Command may disrupt existing ssh connections. Proceed with operation (y|n)?"
+  user "root"
+  command "ufw allow 10022; echo y | ufw enable" # "Command may disrupt existing ssh connections. Proceed with operation (y|n)?"
 end
